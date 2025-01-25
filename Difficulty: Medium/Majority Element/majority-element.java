79b4998f02f37e174e197a2bae329db47775a324
Chandan Sharma <109021997+chandansharma65914@users.noreply.github.com>
2025-01-25 12:08:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            System.out.println(new Solution().majorityElement(arr));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int majorityElement(int arr[]) {
+        // your code here
+        HashMap<Integer,Integer> map= new HashMap<>();
+        for(int x :arr){
+            if(map.containsKey(x)){
+                map.put(x,map.get(x)+1);
+            }else{
+                map.put(x,1);
+            }
+        }
+        for(int x : map.keySet()){
+            if(map.get(x) > arr.length/2){
+                return x;
+            }
+        }
+        return -1;
+    }
+}
