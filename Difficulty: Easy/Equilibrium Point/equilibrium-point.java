1c500ae0fbaba070048e57ee999c09c777c813a8
Chandan Sharma <109021997+chandansharma65914@users.noreply.github.com>
2025-02-01 21:47:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(
+                    token)); // Use Integer.parseInt to parse int values
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+
+            // calling equilibriumPoint() function
+            System.out.println(obj.findEquilibrium(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find equilibrium point in the array.
+    public static int findEquilibrium(int arr[]) {
+        // code here
+        int ans =-1;
+        int sum=0;
+        for(int x :arr){
+            sum+=x;
+        }
+        int rum=0;
+        for(int i=0;i<arr.length;i++){
+            if(2*rum == sum-arr[i]){
+                return i;
+            }
+            rum+=arr[i];
+        }
+        return ans;
+    }
+}
