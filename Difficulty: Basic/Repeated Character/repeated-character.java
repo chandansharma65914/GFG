4b25e91c2296619b9a1e9da91126e4bfebc6f7da
Chandan Sharma <109021997+chandansharma65914@users.noreply.github.com>
2025-03-22 23:57:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Repeated Character/repeated-character.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            String s;
+            s = sc.next();
+            
+            Solution ob = new Solution();
+            char res = ob.firstRep(s);
+            if (res == '#')
+                System.out.println(-1);
+            else
+                System.out.println(res);
+             
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+    char firstRep(String S)
+    {
+       Map<Character,Integer> map= new LinkedHashMap<>();
+       for(char c : S.toCharArray()){
+           if(map.containsKey(c)){
+              map.put(c,map.get(c)+1);
+           }else{
+               map.put(c,1);
+           }
+       }
+       for(char c  : map.keySet()){
+           if(map.get(c)>1){
+               return c;
+           }
+       }
+       return '#';
+    }
+}
