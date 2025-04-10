8a8443e6f741817db7a026ee425204a109f7d586
Chandan Sharma <109021997+chandansharma65914@users.noreply.github.com>
2025-04-10 20:22:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+            String[] nk = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(nk[0]);
+            Solution sln = new Solution();
+            int ans = sln.countFreq(a, k);
+
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    int countFreq(int[] arr, int target) {
+        // code here
+        Map<Integer,Integer> map= new LinkedHashMap<>();
+        for(int x : arr){
+            if(map.containsKey(x)){
+                map.put(x, map.get(x)+1);
+            }else{
+                map.put(x,1);
+            }
+        }
+       
+        if(map.containsKey(target)){
+                return map.get(target);
+            }else{
+                 return 0;
+            }
+    }
+}
